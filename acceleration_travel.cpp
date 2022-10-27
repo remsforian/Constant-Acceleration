a6cf86b9fed1ffa28e8af11a1301f3800e3f0972
remsforian <teddy.masters@protonmail.com>
2022-10-27 11:50:48 -0400

    initial commit

---
+++ acceleration_travel.cpp
@@ -0,0 +1,340 @@
+\\ distanace = (1/2) a t^2 
+\\ a should be converted to m/s
+\\ also need to account for half the time being in deceleration, we can just have const acceleration to the half way point. 
+\\ create a corordinate system (2d) where all the planets lie
+\\ pick a t = 0 position for the planet position.
+\\ allow for an "advanced" mode that would let you put in corordinates. 
+\\ we can assume mose orbits are spherical, so velocity is a function of radius
+\\ Days are the base unit this program uses
+
+#include <iostream>
+#include <cmath>
+#define PI 3.14159265
+#include <vector>
+
+const double msun = 1.989e30; \\mass of the sun
+const double G = 6.6743e-11; \\gravitational constant
+const double t_0 = 1e150; \\time = 0. Large to make initial planet conditions somewhat random
+const double g = 9.808e-3; \\value of 1g of acceleration in km/s^2
+const double c = 299792.458; \\the speed of light in km/s
+
+double mercuryloc(t,o){
+
+double T = 88; \\orbital period in days
+double d = 57e6; \\distance from sun
+double v = sqrt ((G * msun)/d);
+double y = d * sin ((t/2*PI)T);
+double x = d * cos((t/2*PI)/T);
+
+if (o == "x") {
+
+return x;
+
+}
+
+else if (o == "y") {
+
+return y;
+
+}
+
+else if (o == "v") {
+
+return v;
+
+}
+
+else {
+
+return;
+
+}
+
+}
+
+
+double venusloc(t,o){
+
+double T = 224.7; \\orbital period in days
+double d = 108.2e6; \\distance from sun
+double v = sqrt ((G * msun)/d);
+double y = d * sin ((t/2*PI)T);
+double x = d * cos((t/2*PI)/T);
+
+}
+
+double earthloc(t,o){
+
+double T = 365.25; \\orbital period in days
+double d = 149.6e6; \\distance from sun
+double v = sqrt ((G * msun)/d);
+double y = d * sin ((t/2*PI)T); 
+double x = d * cos((t/2*PI)/T);
+
+}
+
+double marsloc(t,o){
+
+double T = 687; \\orbital period in days
+double d = 228e6; \\distance from sun
+double v = sqrt ((G * msun)/d);
+double y = d * sin ((t/2*PI)T);
+double x = d * cos((t/2*PI)/T);
+
+if (o == "x") {
+
+return x;
+
+}
+
+else if (o == "y") {
+
+return y;
+
+}
+
+else if (o == "v") {
+
+return v;
+
+}
+
+else {
+
+return;
+
+}
+
+}
+
+double jupiterloc(t,o){
+
+double T = 4331; \\orbital period in days
+double d = 778.5e6; \\distance from sun
+double v = sqrt ((G * msun)/d);
+double y = d * sin ((t/2*PI)T);
+double x = d * cos((t/2*PI)/T);
+
+
+if (o == "x") {
+
+return x;
+
+}
+
+else if (o == "y") {
+
+return y;
+
+}
+
+else if (o == "v") {
+
+return v;
+
+}
+
+else {
+
+return;
+
+}
+}
+
+double saturnloc(t,o){
+
+double T = 10747; \\orbital period in days
+double d = 1432e6; \\distance from sun
+double v = sqrt ((G * msun)/d);
+double y = d * sin ((t/2*PI)T);
+double x = d * cos((t/2*PI)/T);
+
+if (o == "x") {
+
+return x;
+
+}
+
+else if (o == "y") {
+
+return y;
+
+}
+
+else if (o == "v") {
+
+return v;
+
+}
+
+else {
+
+return;
+
+}
+}
+
+double uranusloc(t,o){
+
+double T = 30589; \\orbital period in days
+double d = 2867e6; \\distance from sun
+double v = sqrt ((G * msun)/d);
+double y = d * sin ((t/2*PI)T);
+double x = d * cos((t/2*PI)/T);
+
+if (o == "x") {
+
+return x;
+
+}
+
+else if (o == "y") {
+
+return y;
+
+}
+
+else if (o == "v") {
+
+return v;
+
+}
+
+else {
+
+return;
+
+}
+}
+
+double neptuneloc(t,o){
+
+double T = 59800; \\orbital period in days
+double d = 4515e6; \\distance from sun
+double v = sqrt ((G * msun)/d);
+double y = d * sin ((t/2*PI)T);
+double x = d * cos((t/2*PI)/T);
+
+if (o == "x") {
+
+return x;
+
+}
+
+else if (o == "y") {
+
+return y;
+
+}
+
+else if (o == "v") {
+
+return v;
+
+}
+
+else {
+
+return;
+
+}
+}
+
+double calculate(v0,vf,x0,xf,y0,yf,a){
+
+double dist = sqrt((xf-x0)*(xf-x0)+(yf-y0)*(yf-y0); \\distance between points 
+double distfab = dist/2; \\distance to flip and burn 
+double ak = a * g; \\acceleration in km/s
+double c0 = sqrt(x0*x0+y0*y0); \\distance from dest to sun
+double cf = sqrt(xf*xf+yf*yf);
+std::vector<double> vv0 = {((y0)/c0)*v0,((x0 * - 1)/c0)*v0}; \\velocity vector of starting point
+std::vector<double> vvf = {((yf)/cf)*vf,((xf * - 1)/cf)*vf}; \\velocity vector of destination
+
+
+}
+
+
+
+
+int main(){
+
+std::cout << "Pick Starting Location \n \"Mercury\" \n \"Venus\" \n \"Earth\" \n \"Mars\" \n \"Jupiter\" \n \"Saturn\" \n \"Uranus\" \n \"Neptune\" \n Coordinate Input \n";
+std::cin >> start; \\get starting location
+
+std::cout << "Pick Ending Location \n \"Mercury\" \n \"Venus\" \n \"Earth\" \n \"Mars\" \n \"Jupiter\" \n \"Saturn\" \n \"Uranus\" \n \"Neptune\" \n Coordinate Input \n";
+std::cin >> end; \\get ending locetion
+
+std::cout << "How long in days has it been since t_0 \n";
+std::cin >> t; \\take time from begining
+int t = t + t_0; \\take into account initial conditions
+
+std::cout << "What is the acceleration in gs \n";
+std::cin >> a; \\get the acceleration value
+
+\\function calls to put variables into main
+
+if (start == "Mercury"){
+
+stx = mercuryloc(t,x); \\begining x position
+sty = mercuryloc(t,y); \\begining y position
+stv = mecruryloc(t,v); \\initial velocity
+
+}
+
+if (start == "Venus"){
+
+stx = venusloc(t,x); \\begining x position
+sty = venusloc(t,y); \\begining y position
+stv = venusloc(t,v); \\initial velocity
+
+}
+
+if (start == "Earth"){
+
+stx = earthloc(t,x); \\begining x position
+sty = earthloc(t,y); \\begining y position
+stv = earthloc(t,v); \\initial velocity
+
+}
+
+if (start == "Mars"){
+
+stx = marsloc(t,x); \\begining x position
+sty = marsloc(t,y); \\begining y position
+stv = marsloc(t,v); \\initial velocity
+
+}
+
+if (start == "Jupiter"){
+
+stx = jupiterloc(t,x); \\begining x position
+sty = jupiterloc(t,y); \\begining y position
+stv = jupiterloc(t,v); \\initial velocity
+
+}
+
+if (start == "Saturn"){
+
+stx = saturnloc(t,x); \\begining x position
+sty = saturnloc(t,y); \\begining y position
+stv = saturnloc(t,v); \\initial velocity
+
+}
+
+if (start == "Uranus"){
+
+stx = uranusloc(t,x); \\begining x position
+sty = uranusloc(t,y); \\begining y position
+stv = uranusloc(t,v); \\initial velocity
+
+}
+
+if (start == "Neptune"){
+
+stx = neptuneloc(t,x); \\begining x position
+sty = neptuneloc(t,y); \\begining y position
+stv = neptuneloc(t,v); \\initial velocity
+
+}
+
+}
